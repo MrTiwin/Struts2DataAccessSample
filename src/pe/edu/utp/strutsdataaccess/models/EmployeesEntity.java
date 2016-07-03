52804ae1eafcbbe65879e9f4a3d2cfdd172f7a38
MrTiwin <1411901@utp.edu.pe>
2016-07-02 21:58:17 -0500

    Initial commit

---
+++ src/pe/edu/utp/strutsdataaccess/models/EmployeesEntity.java
@@ -0,0 +1,45 @@
+package pe.edu.utp.strutsdataaccess.models;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by GrupoUTP on 02/07/2016.
+ */
+public class EmployeesEntity {
+    private Connection connection;
+    private static String DEFAULT_QUERY="SELECT * FROM Employees";
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+    public List<Employee> findAll(){
+        String query = DEFAULT_QUERY;
+        List<Employee> employees = new ArrayList<>();
+        if(getConnection() == null) return null;
+        try {
+            Statement stmt = getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                Employee employee = new Employee(
+                        rs.getString("first_name"),
+                        rs.getString("last_name"));
+                employees.add(employee);
+            }
+            return employees;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public int getEmployeesCount() { return findAll().size();}
+}
+
---
+++ web/showErrors.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: GrupoUTP
+  Date: 02/07/2016
+  Time: 21:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Error Page</title>
+</head>
+<body>
+    There were some Errors
+</body>
+</html>
